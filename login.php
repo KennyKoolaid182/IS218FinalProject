16740dc34de6a36639300295e0e845e35640d230
Kenny Eisenhardt <kce6@njit.edu>
2016-12-21 14:14:05 -0500

    Login form

---
+++ login.php
@@ -0,0 +1,101 @@
+<?php
+//include config file
+require_once('includes/config.php');
+
+//Sends user to homepage if they are logged in
+if( $user->is_logged_in() ){ header('Location: index.php'); } 
+
+//process login form once it is submitted
+if(isset($_POST['submit'])){
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if($user->login($username,$password)){ 
+		$_SESSION['username'] = $username;
+		header('Location: userpage.php');
+		exit;
+	
+	} else {
+		$error[] = 'Wrong username or password or your account has not been activated.';
+	}
+
+}//end of submit
+
+//define page title
+$title = 'Login Page';
+
+//code for setting up header
+require('layout/header.php'); 
+?>
+
+	
+<div class="container">
+
+	<div class="row">
+
+	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+			<form role="form" method="post" action="" autocomplete="off">
+				<h2>Please Login to Your Account</h2>
+				<p><a href='./'>Back to home page</a></p>
+				<hr>
+
+				<?php
+				//checks for errors and echos any errors found
+				if(isset($error)){
+					foreach($error as $error){
+						echo '<p class="bg-danger">'.$error.'</p>';
+					}
+				}
+
+				if(isset($_GET['action'])){
+
+					//returns information to user based on action performed
+					switch ($_GET['action']) {
+						case 'active':
+							echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
+							break;
+						case 'reset':
+							echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
+							break;
+						case 'resetAccount':
+							echo "<h2 class='bg-success'>Password changed, you may now login.</h2>";
+							break;
+					}
+
+				}
+
+				
+				?>
+
+				<div class="form-group">
+					<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo $_POST['username']; } ?>" tabindex="1">
+				</div>
+
+				<div class="form-group">
+					<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+				</div>
+				
+				<div class="row">
+					<div class="col-xs-9 col-sm-9 col-md-9">
+						 <a href='reset.php'>Forgot your Password?</a>
+					</div>
+				</div>
+				
+				<hr>
+				<div class="row">
+					<div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Login" class="btn btn-primary btn-block btn-lg" tabindex="5"></div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+
+</div>
+
+
+<?php 
+//code to set up footer
+require('layout/footer.php'); 
+?>
